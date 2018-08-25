d048a7f36259edc7b4d70ab77eaf1b2deb148a3f
Iago Breno Alves do Carmo Araujo <ibaraujo@poshul.ime.usp.br>
2018-08-25 17:06:05 -0300

    Try to load using JSX

--- components/like_button.js
+++ components/like_button.js
@@ -1,7 +1,3 @@
-'use strict';
-
-const e = React.createElement;
-
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
@@ -13,13 +9,11 @@ class LikeButton extends React.Component {
       return 'You liked this.';
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    return (
+      <button onClick={() => this.setState({ liked: true })}>Like</button>
+    ); 
   }
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<LikeButton />, domContainer);
